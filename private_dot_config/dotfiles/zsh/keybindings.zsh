d00dd2bc4d00a98d67746153c8c2975c3b7473e3
papanito <papanito@wyssmann.com>
2022-12-10 22:16:54 +0100

    feat: add keybindings for multiline commands

--- private_dot_config/dotfiles/zsh/keybindings.zsh
+++ private_dot_config/dotfiles/zsh/keybindings.zsh
@@ -188,3 +188,15 @@ bindkey -- "${keys[PageDown]}"        .end-of-buffer-or-history
 bindkey -- "${keys[Shift+Tab]}"       .reverse-menu-complete
 bindkey -- "${keys[Ctrl+Left]}"       .backward-word
 bindkey -- "${keys[Ctrl+Right]}"      .forward-word
+
+# edit multiline commands using $EDITOR
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^X^X" edit-command-line
+
+# set editor default keymap to emacs
+bindkey "^A" vi-beginning-of-line
+bindkey "^E" vi-end-of-line
+
+# prevent delete key from inserting ~
+bindkey "^[[3~" delete-char
