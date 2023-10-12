d2933dd827fa960558376c012d82da513402270f
papanito <papanito@wyssmann.com>
2023-10-12 16:53:08 +0200

    feat: add wezterm config

---
+++ private_dot_config/wezterm/extra/ssh.lua
@@ -0,0 +1,12 @@
+return {
+   {
+      -- This name identifies the domain
+      name = 'rss',
+      -- The hostname or address to connect to. Will be used to match settings
+      -- from your ssh config file
+      remote_address = 'rss.wyssmann.dev',
+      -- The username to use on the remote host
+      username = 'ansible',
+      multiplexing = 'None',
+  }
+}
