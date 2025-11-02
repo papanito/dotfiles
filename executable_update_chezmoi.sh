cc2fbbacaaccfc97a42d35b3aca72697141f7b48
papanito <papanito@wyssmann.com>
2025-11-02 16:07:55 +0100

    Update script update_chezmoi.sh

--- executable_update_chezmoi.sh
+++ executable_update_chezmoi.sh
@@ -3,6 +3,7 @@ echo "# Update home-manager flake"
 pushd ~/.config/home-manager
 nix flake update
 popd
+chezmoi add ~/.config/home-manager/flake.lock
 
 echo "# Add some files which might be outdaed"
 chezmoi add ~/.config/nvim/lazyvim.json
