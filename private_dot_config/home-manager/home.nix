9b668932b32b9a0e2b96ec463c516f86dbe86918
papanito <papanito@wyssmann.com>
2026-03-26 20:17:02 +0100

    home-manager: Add more tools

--- private_dot_config/home-manager/home.nix
+++ private_dot_config/home-manager/home.nix
@@ -1,17 +1,29 @@
-{  pkgs, lib, config, nixgl, quickshell, home_attrs, security, ... }:
-let 
+{
+  pkgs,
+  lib,
+  config,
+  nixgl,
+  quickshell,
+  home_attrs,
+  security,
+  ...
+}:
+let
   mountdir_yunohost = "${config.home.homeDirectory}/cs/yuno";
-  my-python-packages = ps: with ps; [
-    pandas
-    requests
-    pip # The PyPA recommended tool for installing Python packages
-    django
-    pillow # The friendly PIL fork (Python Imaging Library)
-    jupyter # A high-level dynamically-typed programming language
-    notebook # Web-based notebook environment for interactive computing
-  ];
-  my-nodes-packages = ns: with ns; [
-  ];
+  my-python-packages =
+    ps: with ps; [
+      pandas
+      requests
+      pip # The PyPA recommended tool for installing Python packages
+      django
+      pillow # The friendly PIL fork (Python Imaging Library)
+      jupyter # A high-level dynamically-typed programming language
+      notebook # Web-based notebook environment for interactive computing
+    ];
+  my-nodes-packages =
+    ns: with ns; [
+      prettier-plugin-toml
+    ];
 in
 {
   home = {
@@ -46,11 +58,12 @@ in
     nix-direnv # A fast, persistent use_nix implementation for direnv
     colmena
     statix
+    dconf2nix
 
     ## AI
-    qwen-code #Coding agent that lives in digital world
+    qwen-code # Coding agent that lives in digital world
     ollama
-    
+
     ## Security
     tirith # URL security analysis for shell environments
     trivy
@@ -62,20 +75,20 @@ in
     pueue
     lutris # Open Source gaming platform for GNU/Linux
     goto # easy to use terminal SSH manager with advanced features. Binaries included!
-    
+
     ## Development
     act # Run your GitHub Actions locally
     actionlint # Static checker for GitHub Actions workflow files
     bump # CLI tool to draft a GitHub Release for the next semantic version
     buildah # A tool which facilitates building OCI images
     buildkit # Concurrent, cache-efficient, and Dockerfile-agnostic builder toolkit
-    buildkit-nix #Nix frontend for x
+    buildkit-nix # Nix frontend for x
     buildkite-cli # A command line interface for Buildkite
     dbeaver-bin # Universal SQL Client for developers, DBA and analysts. Supports MySQL, PostgreSQL, MariaDB, SQLite, and more
     dbgate # Database manager for MySQL, PostgreSQL, SQL Server, MongoDB, SQLite and others
     shellcheck # Shell script analysis tool
     skaffold
-    uv #Extremely fast Python package installer and resolver, written in Rust
+    uv # Extremely fast Python package installer and resolver, written in Rust
     gemini-cli-bin # AI agent that brings the power of Gemini
     geminicommit # CLI that generates git commit messages with Google Gemini AI
     codeberg-cli
@@ -91,10 +104,13 @@ in
     nodePackages.snyk # snyk library and cli utility
     # obsidian # A powerful knowledge base that works on top of a local folder of plain text Markdown files
     rustup # Rust toolchain installer
+    prettier # Code formatter
+    prettier-plugin-go-template # Fixes prettier formatting for go templates
+    commitlint
 
     ### API
     bruno # Open-source IDE For exploring and testing APIs
-    hurl #Command line tool that performs HTTP requests defined in a simple plain text format.
+    hurl # Command line tool that performs HTTP requests defined in a simple plain text format.
     insomnia # The most intuitive cross-platform REST API Client
     posting
     aria2 # Lightweight, multi-protocol, multi-source, command-line download utility
@@ -106,7 +122,7 @@ in
     gocryptfs # Encrypted overlay filesystem written in Go
     cryptor # Simple gocryptfs GUI
     vaults # GTK frontend for encrypted vaults supporting gocryptfs and CryFS for encryptiongocryptfs
-    steghide #Open source steganography program
+    steghide # Open source steganography program
     tomb # File encryption on GNU/Linux
 
     ## Terminal
@@ -118,7 +134,7 @@ in
     vhs # Tool for generating terminal GIFs with code
     yazi # terminal file explorer
     guake
-    
+
     ## Docker and Kubernetes
     dive # Tool for exploring each layer in a docker image
 
@@ -134,11 +150,10 @@ in
     terraform
     #terragrunt # A thin wrapper for Terraform that supports locking for Terraform state and enforces best practices
     terraform-docs # A utility to generate documentation from Terraform modules in various output formats
-    tflint 
+    tflint
     packer
     vagrant
-    
-    
+
     ## fun
     genact # Nonsense activity generator
     nms # A command line tool that recreates the famous data decryption effect seen in the 1992 movie Sneakers
@@ -149,7 +164,7 @@ in
     gnome-network-displays # miracast implementation for GNOME
     gnomeExtensions.tailscale-qs # Add Tailscale to GNOME quick settings
     gnomeExtensions.bing-wallpaper-changer
-    gnomeExtensions.keep-awake # Keep your computer awake! Prevents that your computer activates sceensaver, turns off screen(s) or goes to hibernate when not actively used for a while. 
+    gnomeExtensions.keep-awake # Keep your computer awake! Prevents that your computer activates sceensaver, turns off screen(s) or goes to hibernate when not actively used for a while.
     gnomeExtensions.gsconnect
     gnomeExtensions.top-bar-organizer
     gnomeExtensions.topiconsfix # Shows legacy tray icons on top – the fixed version of https://extensions.gnome.org/extension/495/topicons/
@@ -167,7 +182,7 @@ in
     gnomeExtensions.hue-lights # This extension controls Philips Hue compatible lights using Philips Hue Bridge on your local network, it also allows controlling Philips Hue Sync Box. I
     gnomeExtensions.forge # Tiling and window manager for GNOME
     gnomeExtensions.display-configuration-switcher # Quickly change the display configuration from the system menu.
-    gnomeExtensions.another-window-session-manager # Close open windows gracefully and save them as a session. 
+    gnomeExtensions.another-window-session-manager # Close open windows gracefully and save them as a session.
     gnomeExtensions.window-state-manager # Automatically remember and restore window state and positions.
     gnomeExtensions.dash-to-panel
 
@@ -181,16 +196,16 @@ in
     evince
     rssguard
     vivaldi # browser
-    poppler-utils #PDF
+    poppler-utils # PDF
     nextcloud-client
     protonvpn-gui
     speechd # Common interface to speech synthesis
-    
+
     ## Media
     sweethome3d.application # Design and visualize your future home
     sweethome3d.textures-editor # Easily create SH3T files and edit the properties of the texture images it contain
-    sweethome3d.furniture-editor #Quickly create SH3F files and edit the properties of the 3D models it contain
-    freecad 
+    sweethome3d.furniture-editor # Quickly create SH3F files and edit the properties of the 3D models it contain
+    freecad
     pinta
     gimp
     #gimpPlugins.gap
