5898acd824efa2bc81d010f07fbc45a51b416de8
papanito <papanito@wyssmann.com>
2026-06-04 21:05:48 +0200

    Fix some zsh stuff

--- private_dot_config/zshrc.d/dots-hyprland.zsh
+++
@@ -1,5 +0,0 @@
-# Use the generated color scheme
-
-if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt; then
-    cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-fi
