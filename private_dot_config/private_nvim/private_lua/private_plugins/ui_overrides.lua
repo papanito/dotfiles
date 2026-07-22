97deebb1dba096ed194a5aea60e86226baacaf07
papanito <papanito@wyssmann.com>
2026-07-22 15:37:16 +0200

    fix: Terminal ESC

--- private_dot_config/private_nvim/private_lua/private_plugins/ui_overrides.lua
+++ private_dot_config/private_nvim/private_lua/private_plugins/ui_overrides.lua
@@ -10,6 +10,30 @@ return {
           explorer = { hidden = true }, -- Snacks file explorer sidebar
         },
       },
+      -- Override terminal double-ESC window from 200ms (default) to 500ms
+      -- so exiting terminal mode doesn't require pressing ESC very fast.
+      styles = {
+        terminal = {
+          keys = {
+            term_normal = {
+              "<esc>",
+              function(self)
+                self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
+                if self.esc_timer:is_active() then
+                  self.esc_timer:stop()
+                  vim.cmd("stopinsert")
+                else
+                  self.esc_timer:start(500, 0, function() end)
+                  return "<esc>"
+                end
+              end,
+              mode = "t",
+              expr = true,
+              desc = "Double escape to normal mode",
+            },
+          },
+        },
+      },
     },
   },
 }
