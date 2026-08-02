281a99b156f8aa62177f36643a2097d533ef38e5
papanito <papanito@wyssmann.com>
2026-08-02 11:29:44 +0200

    fix: Add SSL_CERT variables for omniroute

--- private_dot_config/private_home-manager/private_modules/omniroute.nix
+++ private_dot_config/private_home-manager/private_modules/omniroute.nix
@@ -13,6 +13,8 @@
       Environment = [
         "OMNIROUTE_HOST=127.0.0.1"
         "OMNIROUTE_PORT=20128"
+        "SSL_CERT_DIR=/etc/ssl/certs"
+        "SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt"
       ];
       Restart = "on-failure";
       RestartSec = 5;
@@ -23,4 +25,3 @@
     };
   };
 }
-
